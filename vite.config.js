821535690d6dc80f21aa6063c93063015b127396
Andréia Bica <122941582+podeserdeia@users.noreply.github.com>
2025-05-18 12:55:33 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://podeserdeia.github.io/Primeiro_smart_control/', // Coloque o nome do seu repositório aqui
+  base: '/Primeiro_smart_control/',
   build: {
-    outDir: 'dist', // Ou qualquer outra pasta de saída que você esteja usando
-    assetsDir: 'assets', // Opcional: se você tiver uma pasta de assets específica
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 });
